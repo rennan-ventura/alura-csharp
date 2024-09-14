809ff44bd54c172e0ca6b7373daa11f79cb894d7
Rennan Ventura <rennan.ventura.alves@gmail.com>
2024-09-14 02:20:36 -0300

    resolução ex1

--- csharp-01/Program.cs
+++ csharp-01/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 Console.WriteLine(mensagemDeBoasVindas);
+
+Console.WriteLine("Digite uma nota");
+String? linha = Console.ReadLine();
+int notaMedia = int.Parse(linha);
+
+if (notaMedia <= 5){
+    Console.WriteLine("Nota insuficiente!");
+} else {
+    Console.WriteLine("Você passou!!!");
+}   
